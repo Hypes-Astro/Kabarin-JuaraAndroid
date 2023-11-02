db8d3cb8ffd6036e32712e8d22b1ed610d6693b6
Hypes-Astro <muhamadalifanwar@gmail.com>
2023-11-02 13:52:46 +0700

    Continue Home Screen ViewModel - Article card

--- app/src/main/java/com/loc/newsapp/util/Constants.kt
+++ app/src/main/java/com/loc/newsapp/util/Constants.kt
@@ -1,6 +1,11 @@
 package com.loc.newsapp.util
 
 object Constants {
+
     const val USER_SETTINGS = "user_settings"
     const val APP_ENTRY = "app_entry"
+
+    const val BASE_URL = "https://newsapi.org/v2/"
+    const val API_KEY = "b2949a4c5533464b92d35278842419a6"
+
 }
