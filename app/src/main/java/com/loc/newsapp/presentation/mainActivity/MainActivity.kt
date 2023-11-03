1b88add2d3f2ee79fcf23d5786fe25a7fbfd64c3
Hypes-Astro <muhamadalifanwar@gmail.com>
2023-11-03 21:08:14 +0700

    finished setup database

--- app/src/main/java/com/loc/newsapp/presentation/mainActivity/MainActivity.kt
+++ app/src/main/java/com/loc/newsapp/presentation/mainActivity/MainActivity.kt
@@ -23,6 +23,9 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.lifecycle.lifecycleScope
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.loc.newsapp.data.local.NewsDao
+import com.loc.newsapp.domain.model.Article
+import com.loc.newsapp.domain.model.Source
 import com.loc.newsapp.domain.usecases.app_entry.AppEntryUseCases
 import com.loc.newsapp.presentation.navgraph.NavGraph
 import com.loc.newsapp.presentation.onboarding.OnBoardingScreen
@@ -35,9 +38,11 @@ import javax.inject.Inject
 class MainActivity : ComponentActivity() {
 
     @Inject
-    lateinit var useCases: AppEntryUseCases
-    private val viewModel by viewModels<MainViewModel>()
+   lateinit var useCases: AppEntryUseCases
 
+    private val viewModel by viewModels<MainViewModel>()
+    @Inject
+    lateinit var dao: NewsDao
 
 
 
@@ -47,11 +52,31 @@ class MainActivity : ComponentActivity() {
         installSplashScreen().apply {
             setKeepOnScreenCondition(condition = { viewModel.splashCondition.value })
         }
+
+        lifecycleScope.launch {
+            dao.upsert(
+                Article(
+                    author = "",
+                    title = "Coinbase says Apple blocked its last app release on NFTs in Wallet ... - CryptoSaurus",
+                    description = "Coinbase says Apple blocked its last app release on NFTs in Wallet ... - CryptoSaurus",
+                    content = "We use cookies and data to Deliver and maintain Google services Track outages and protect against spam, fraud, and abuse Measure audience engagement and site statistics to unde… [+1131 chars]",
+                    publishedAt = "2023-06-16T22:24:33Z",
+                    source = Source(
+                        id = "", name = "bbc"
+                    ),
+                    url = "https://consent.google.com/ml?continue=https://news.google.com/rss/articles/CBMiaWh0dHBzOi8vY3J5cHRvc2F1cnVzLnRlY2gvY29pbmJhc2Utc2F5cy1hcHBsZS1ibG9ja2VkLWl0cy1sYXN0LWFwcC1yZWxlYXNlLW9uLW5mdHMtaW4td2FsbGV0LXJldXRlcnMtY29tL9IBAA?oc%3D5&gl=FR&hl=en-US&cm=2&pc=n&src=1",
+                    urlToImage = "https://media.wired.com/photos/6495d5e893ba5cd8bbdc95af/191:100/w_1280,c_limit/The-EU-Rules-Phone-Batteries-Must-Be-Replaceable-Gear-2BE6PRN.jpg"
+                )
+            )
+        }
+
         lifecycleScope.launch {
             useCases.readAppEntry().collect{
                 Log.d("test",it.toString())
             }
         }
+
+
         setContent {
             NewsAppTheme(dynamicColor = false) {
                 val isSystemInDarkMode = isSystemInDarkTheme()
